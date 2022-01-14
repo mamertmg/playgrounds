0f8f0540654452ea0aec8d720b3957a392d7381c
Wutao <wutao.wen@gmail.com>
2022-01-14 19:13:39 +0100

    landing page mock up

--- map-styling/script.js
+++ map-styling/script.js
@@ -1,4 +1,13 @@
-function initMap() {
+async function initMap() {
+
+    // const markers = async (index) => {
+
+    //     const res = await axios.get('https://opendata.duesseldorf.de/api/action/datastore/search.json?resource_id=6d19eaa3-9967-4af4-b4b6-b2bcebe301a2&limit=5&fields[]=objektbezeichnung&fields[] =latitude&fields[]=longitude&fields[]=objektart&fields[]=strasse_hausnr')
+    //     arr = res.data.result.records
+    //     return arr[index]
+    // }
+
+
 
     const map = new google.maps.Map(document.getElementById("map"), {
         mapId: "546de97e85a128a6",
@@ -6,36 +15,46 @@ function initMap() {
         zoom: 15,
     });
 
+
+
+
+
+
     const markers = [
-        ["Seilspielplatz", 51.232928391458785, 6.7731710355280015, "Bolzplatz", 5],
-        ["Spielplatz Spee‘scher Graben", 51.221624427602734, 6.77031014027715, "Kinderspielplatz", 4],
-        ["Spielplatz Schwanenmarkt", 51.22014623354839, 6.773571536911311, "Bolzplatz", 3],
-        ["Spielplatz im WGZ Bank-Park", 51.2187728973201, 6.798678328408502, "Bolzplatz", 5],
-        ["Spielplatz Lessingplatz", 51.21409645756707, 6.793783676859652, "Kinderspielplatz", 4],
+        { objektbezeichung: "Seilspielplatz", latitude: 51.232928391458785, longitude: 6.7731710355280015, objektart: "Bolzplatz", strasse_hausnr: 5 },
+        { objektbezeichung: "Spielplatz Spee‘scher Graben", latitude: 51.221624427602734, longitude: 6.77031014027715, objektart: "Kinderspielplatz", strasse_hausnr: 4 },
+        { objektbezeichung: "Spielplatz Schwanenmarkt", latitude: 51.22014623354839, longitude: 6.773571536911311, objektart: "Bolzplatz", strasse_hausnr: 3 },
+        { objektbezeichung: "Spielplatz im WGZ Bank-Park", latitude: 51.2187728973201, longitude: 6.798678328408502, objektart: "Bolzplatz", strasse_hausnr: 5 },
+        { objektbezeichung: "Spielplatz Lessingplatz", latitude: 51.21409645756707, longitude: 6.793783676859652, objektart: "Kinderspielplatz", strasse_hausnr: 4 },
     ];
 
 
-    for (let i = 0; i < markers.length; i++) {
+
+    for (let i = 0; i < 5; i++) {
+
         const currMarker = markers[i];
         const currMarkerImgSrc = `"https://picsum.photos/200?random=${i}"`;
-        const marker = new google.maps.Marker({
-            position: { lat: currMarker[1], lng: currMarker[2] },
-            map,
-            title: currMarker[0],
-            icon: {
-                url: "playground-pixel.png",
-                scaledSize: new google.maps.Size(40, 40)
-            },
-            animation: google.maps.Animation.DROP,
-        });
+        const marker = new google.maps.Marker(
+
+            {
+
+                position: { lat: currMarker.latitude, lng: currMarker.longitude },
+                map,
+                title: currMarker.objektbezeichung,
+                icon: {
+                    url: "playground-pixel.png",
+                    scaledSize: new google.maps.Size(40, 40)
+                },
+                animation: google.maps.Animation.DROP,
+            });
 
 
-        const contentString = `
+        const contentString = ` 
         <div id='currMarkerImg'><a href='www.google.com'><img src=${currMarkerImgSrc}></a></div>
-        <b>${currMarker[0]}</b>
+        <b> ${currMarker.objektbezeichung}</b>
         <p id='currMarkerDescr'>
-        <div id='currMarkerRating'>Adresse: ${currMarker[4]}</div> 
-        Type: ${currMarker[3]}</p>`;
+        <div id='currMarkerRating'>Adresse: ${currMarker.strasse_hausnr}</div> 
+        Type: ${currMarker.objektart}</p>`;
 
         const infowindow = new google.maps.InfoWindow({
             content: contentString
@@ -49,4 +68,20 @@ function initMap() {
     }
 
 
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+
+
+            document.querySelector('#searchbox').value = `lat: ${pos.coords.latitude}, lng: ${pos.coords.longitude}`
+
+
+
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
 }
