7409888b6902d421f6ccec414edf6ec9ba217b6f
Wutao <wutao.wen@gmail.com>
2022-02-18 21:42:28 +0100

    mobile view for landing page

--- map-styling/landing page/public/app.js
+++ map-styling/landing page/public/app.js
@@ -1,6 +1,6 @@
-//top navbar turn white on scroll
+//top navbar turn white on scroll, bottom nabar disappears at bottom
 
-const nav = document.querySelector("nav");
+const navTop = document.querySelector("nav");
 const sectionOne = document.querySelector(".top-container");
 
 const sectionOneOptions = {
@@ -13,11 +13,10 @@ const sectionOneObserver = new IntersectionObserver(function (
 ) {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
-            nav.classList.add("bg-white");
+            navTop.classList.add("bg-white");
         } else {
-            console.log(entry.target);
 
-            nav.classList.remove("bg-white");
+            navTop.classList.remove("bg-white");
         }
     });
 },
@@ -26,15 +25,36 @@ const sectionOneObserver = new IntersectionObserver(function (
 sectionOneObserver.observe(sectionOne);
 
 
+const navBottom = document.querySelector(".fixed-bottom");
+const sectionTwo = document.querySelector(".bottom-container");
+
+const sectionTwoOptions = {
+    threshold: 0.1
+};
+
+const sectionTwoObserver = new IntersectionObserver(function (
+    entries,
+    sectionTwoObserver
+) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            navBottom.classList.add("visually-hidden");
+        } else {
+
+            navBottom.classList.remove("visually-hidden");
+        }
+    });
+},
+    sectionTwoOptions);
+
+sectionTwoObserver.observe(sectionTwo);
+
+
+
+
 //Autocomplete
 
 let autocomplete;
-
-const southwest = { lat: 5.6108, lng: 136.589326 };
-const northeast = { lat: 61.179287, lng: 2.64325 };
-const newBounds = new google.maps.LatLngBounds(southwest, northeast);
-
-
 
 function initAutocomplete() {
 
@@ -49,17 +69,65 @@ function initAutocomplete() {
 
 }
 
+// autocompleted address for backend
+let addressQuery;
 function onPlaceChanged() {
     let place = autocomplete.getPlace();
     if (!place.geometry) {
-        // user didnt choose a prediction, reset the input field
         document.getElementById('autocomplete').placeholder = 'Enter a place';
     }
     else {
-        document.getElementById('details').innerHTML = place.name;
+        let addressQuery = place.name;
     }
 }
 
+// Swiper
 
+const swiper = new Swiper('.mySwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            axios
+                .get(
+                    `https://nominatim.openstreetmap.org/reverse?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&format=json&limit=1`
+                )
+                .then((response) => {
+                    const { city, road } = response.data.address;
+                    searchInput.value = `${city} ${road}`;
+                    getPlaygrounds(
+                        pos.coords.latitude,
+                        pos.coords.longitude,
+                        labelSelect.value,
+                        limitInput.value,
+                        distInput.value
+                    );
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+document.onscroll = function () {
+    if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+        document.getElementById('nav-bottom-mobile').style.display = 'none';
+    } else {
+        document.getElementById('nav-bottom-mobile').style.display = 'block';
+    }
+}
 
 
