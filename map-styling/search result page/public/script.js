8c5d40e5d06075df97ce6a3396cdbe0fa672b0b0
Wutao <wutao.wen@gmail.com>
2022-03-08 21:08:44 +0100

    added autocomplete to search result page ; restructured filter; added tablet view to search result page

--- map-styling/search result page/public/script.js
+++ map-styling/search result page/public/script.js
@@ -1,78 +1,23 @@
-async function initMap() {
 
-    const map = new google.maps.Map(document.getElementById("mapInResultPage"), {
-        mapId: "546de97e85a128a6",
-        center: { lat: 51.22, lng: 6.77 },
-        zoom: 15,
-    });
+let autocomplete;
 
+async function initAutocomplete() {
 
-    const markers = [
-        { objektbezeichung: "Seilspielplatz", latitude: 51.232928391458785, longitude: 6.7731710355280015, objektart: "Bolzplatz", strasse_hausnr: 5 },
-        { objektbezeichung: "Spielplatz Spee‘scher Graben", latitude: 51.221624427602734, longitude: 6.77031014027715, objektart: "Kinderspielplatz", strasse_hausnr: 4 },
-        { objektbezeichung: "Spielplatz Schwanenmarkt", latitude: 51.22014623354839, longitude: 6.773571536911311, objektart: "Bolzplatz", strasse_hausnr: 3 },
-        { objektbezeichung: "Spielplatz im WGZ Bank-Park", latitude: 51.2187728973201, longitude: 6.798678328408502, objektart: "Bolzplatz", strasse_hausnr: 5 },
-        { objektbezeichung: "Spielplatz Lessingplatz", latitude: 51.21409645756707, longitude: 6.793783676859652, objektart: "Kinderspielplatz", strasse_hausnr: 4 },
-    ];
-
-
-
-    for (let i = 0; i < 5; i++) {
-
-        const currMarker = markers[i];
-        const currMarkerImgSrc = `"https://picsum.photos/200?random=${i}"`;
-        const marker = new google.maps.Marker(
-
-            {
-                position: { lat: currMarker.latitude, lng: currMarker.longitude },
-                map,
-                title: currMarker.objektbezeichung,
-
-                animation: google.maps.Animation.DROP,
-            });
-
-
-        const contentString = ` 
-        <div id='currMarkerImg'><a href='www.google.com'><img src=${currMarkerImgSrc}></a></div>
-        <b> ${currMarker.objektbezeichung}</b>
-        <p id='currMarkerDescr'>
-        <div id='currMarkerRating'>Adresse: ${currMarker.strasse_hausnr}</div> 
-        Type: ${currMarker.objektart}</p>`;
-
-        const infowindow = new google.maps.InfoWindow({
-            content: contentString
+    autocomplete = new google.maps.places.Autocomplete(document.querySelector(".autocomplete"),
+        {
+            componentRestrictions: { 'country': ['de'] },
+            fields: ['geometry', 'name', 'place_id'],
+            types: ['address'],
         });
 
-        marker.addListener("click", () => {
-            infowindow.open(map, marker);
-        })
-
-
-    }
+    autocomplete.addListener('place_changed', onPlaceChanged);
 
 
 }
-
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (pos) {
-
-
-            document.querySelector('#searchbox').value = `lat: ${pos.coords.latitude}, lng: ${pos.coords.longitude}`
-
-
-
-        });
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
-
 
 //top navbar turn white on scroll, bottom nabar disappears when reaching bottom
 
-const navTop = document.querySelector(".fixed-top");
+const mapButton = document.querySelector("#mapButton");
 const sectionOne = document.querySelector(".top-container");
 
 const sectionOneOptions = {
@@ -84,11 +29,10 @@ const sectionOneObserver = new IntersectionObserver(function (
     sectionOneObserver
 ) {
     entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            navTop.classList.add("bg-white");
+        if (entry.isIntersecting) {
+            mapButton.classList.add("visually-hidden");
         } else {
-
-            navTop.classList.remove("bg-white");
+            mapButton.classList.remove("visually-hidden");
         }
     });
 },
@@ -141,24 +85,73 @@ filters.addEventListener('hidden.bs.offcanvas', e => {
     }
 })
 
+async function initMap() {
+
+    const map = new google.maps.Map(document.getElementById("mapInResultPage"), {
+        mapId: "546de97e85a128a6",
+        center: { lat: 51.22, lng: 6.77 },
+        zoom: 15,
+    });
 
 
-//Autocomplete
+    const markers = [
+        { objektbezeichung: "Seilspielplatz", latitude: 51.232928391458785, longitude: 6.7731710355280015, objektart: "Bolzplatz", strasse_hausnr: 5 },
+        { objektbezeichung: "Spielplatz Spee‘scher Graben", latitude: 51.221624427602734, longitude: 6.77031014027715, objektart: "Kinderspielplatz", strasse_hausnr: 4 },
+        { objektbezeichung: "Spielplatz Schwanenmarkt", latitude: 51.22014623354839, longitude: 6.773571536911311, objektart: "Bolzplatz", strasse_hausnr: 3 },
+        { objektbezeichung: "Spielplatz im WGZ Bank-Park", latitude: 51.2187728973201, longitude: 6.798678328408502, objektart: "Bolzplatz", strasse_hausnr: 5 },
+        { objektbezeichung: "Spielplatz Lessingplatz", latitude: 51.21409645756707, longitude: 6.793783676859652, objektart: "Kinderspielplatz", strasse_hausnr: 4 },
+    ];
 
-let autocomplete;
 
-function initAutocomplete() {
 
-    autocomplete = new google.maps.places.Autocomplete(document.querySelector("#autocompleteInResultPage"),
-        {
-            componentRestrictions: { 'country': ['de'] },
-            fields: ['geometry', 'name', 'place_id'],
-            types: ['address'],
+    for (let i = 0; i < 5; i++) {
+
+        const currMarker = markers[i];
+        const currMarkerImgSrc = `"https://picsum.photos/200?random=${i}"`;
+        const marker = new google.maps.Marker(
+
+            {
+                position: { lat: currMarker.latitude, lng: currMarker.longitude },
+                map,
+
+                title: currMarker.objektbezeichung,
+
+                animation: google.maps.Animation.DROP,
+            });
+
+
+        const contentString = ` 
+        <div id='currMarkerImg'><a href='www.google.com'><img src=${currMarkerImgSrc}></a></div>
+        <b> ${currMarker.objektbezeichung}</b>
+        <p id='currMarkerDescr'>
+        <div id='currMarkerRating'>Adresse: ${currMarker.strasse_hausnr}</div> 
+        Type: ${currMarker.objektart}</p>`;
+
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString
         });
 
-    autocomplete.addListener('place_changed', onPlaceChanged);
+        marker.addListener("click", () => {
+            infowindow.open(map, marker);
+        })
+
+
+    }
+
+
+
+
+
+    initAutocomplete();
 
 }
+
+
+
+
+
+
+
 
 // pass autocompleted address as value
 let addressQuery;
@@ -209,16 +202,7 @@ function getLocation() {
 
 // Swiper
 
-const swiper = new Swiper('.mySwiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+
 
 //dropdown checkbox
 
