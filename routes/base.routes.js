ae4e09964de907bcda99b4f2810dc6faeb934db7
akyak <akyak@web.de>
2022-03-19 13:35:30 +0100

    Added playground type filter route. Some small fixes on detail page.

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -15,10 +15,10 @@ router.get(
         if (Object.keys(req.query).length === 0) {
             return res.redirect('/');
         }
-        const { q, lat, lng, dist, labels } = req.query;
+        const { q, lat, lng, dist, labels, type } = req.query;
         // Valid call to search route must have at least the following params:
-        // 1.) if there is only 1 param, it must be labels with ONE valid label
-        if (Object.keys(req.query).length === 1 && !labels) {
+        // 1.) if there is only 1 param, it must be labels or type with ONE valid label
+        if (Object.keys(req.query).length === 1 && (!labels && !type)) {
             return res.redirect('/');
         } else if (Object.keys(req.query).length === 1 && labels) {
             if (
@@ -27,6 +27,26 @@ router.get(
             ) {
                 return res.redirect('/');
             }
+        } else if (Object.keys(req.query).length === 1 && type) {
+            Playground.distinct('type', function (err, types) {
+                if (err) {
+                    res.status(500).render('shared/500');
+                } else {
+                    // redirect to landing page if invalid type
+                    if (!types.includes(type)) {
+                        return res.redirect('/');
+                    }
+                    res.render('base/searchResultPage', {
+                        q: q ? q : '',
+                        dist: dist ? dist : '5',
+                        types,
+                        playgroundLabels,
+                        playgroundEquipment,
+                        label: labels,
+                        type: type
+                    });
+                }
+            });
         }
         // 2.) Otherwise: at least q and dist OR lat, lng and dist
         else if (
@@ -34,7 +54,7 @@ router.get(
             (lat && !lng) ||
             !((q || (lat && lng)) && dist)
         ) {
-            res.redirect('/');
+            return res.redirect('/');
         }
         // Collect all labels from DB for rendering landing page
         Playground.distinct('type', function (err, types) {
@@ -48,6 +68,7 @@ router.get(
                     playgroundLabels,
                     playgroundEquipment,
                     label: labels,
+                    type: type
                 });
             }
         });
